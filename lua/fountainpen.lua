acdacaf9b63dc113991ff6a3af8b64dc1cfe2486
Uri Arev <uri.arev@gmail.com>
2021-02-04 11:00:27 +0200

    Trying to implement feat: PrintVal

--- lua/fountainpen.lua
+++ lua/fountainpen.lua
@@ -1,7 +1,9 @@
 Red = "#FF0000"
 Green = "#00FF00"
 Blue = "#0000FF"
-function PrintVal(value)
+function PrintVal(value,...)
+	local args = {...}
+	print(args)
 	if value == "Red" then
 		print(Red)
 	end
